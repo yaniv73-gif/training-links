864d1a2195dcee7d3543a76a287a01b23b0dff0a
yaniv73-gif <yaniv73@gmail.com>
2026-06-20 12:40:44 +0300

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'fightlab-v1';
+const ASSETS = [
+  '/training-links/',
+  '/training-links/index.html',
+  '/training-links/logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
